9974447cbb7fbfa7f7396f4326768412edeb7f00
mikesternk <mikesternk@gmail.com>
2024-03-07 15:29:04 -0600

    Displaying Information

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// Get the select div from index.html
 const dogBreeds = document.getElementById("breedSelect");
 // console.log(dogBreeds);
 
-const getDogBreeds = async () => {
-  const url = "https://api.thedogapi.com/v1/breeds";
+// Get the information div from index.html
+const breedInfo = document.getElementById("breedInfo");
 
+// Dog API URL
+const url = "https://api.thedogapi.com/v1/breeds";
+
+const getDogBreeds = async () => {
   try {
     const response = await fetch(url);
     console.log(response);
@@ -28,3 +33,40 @@ const getDogBreeds = async () => {
 };
 
 getDogBreeds();
+
+dogBreeds.addEventListener("change", async () => {
+  const selectedBreedId = dogBreeds.value;
+
+  if (selectedBreedId) {
+    try {
+      // Replace the information with blank
+      breedInfo.innerHTML = "";
+
+      const res = await fetch(`${url}/${selectedBreedId}`);
+      //   console.log(res);
+
+      if (!res.ok) {
+        throw new Error("could not fetch resourse");
+      }
+      // Create new Div for dog information
+      const information = document.createElement("div");
+
+      const dogInfo = await res.json();
+      //   console.log(dogInfo);
+
+      information.innerHTML = `
+        <h3>Name: ${dogInfo.name}</h3> 
+        <p><b>Bred For:</b> ${dogInfo.bred_for}</p>
+        <p><b>Temperament:</b> ${dogInfo.temperament}</p>
+        <p><b>Life Span</b> ${dogInfo.life_span}</p>
+        <p><b>Weight:</b> ${dogInfo.weight} kgs</p>
+        `;
+
+      // Display the information
+      breedInfo.appendChild(information);
+      console.log(dogInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
